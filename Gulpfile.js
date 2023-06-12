85133170050f44e5d597cf59fbe2e35402fe350f
xada <wahabzada00@gmail.com>
2023-06-12 14:05:46 +0300

    "commit"

--- Gulpfile.js
+++ Gulpfile.js
@@ -28,23 +28,32 @@
 
 
 
-
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-  return gulp.src('assets/scss/*.scss')
+  return gulp.src('assets/scss/*.scss') // Path to Sass files
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('assets/css'));
+    .pipe(gulp.dest('output/css')) // Path to output directory for CSS files
+    .pipe(browserSync.stream());
 });
 
-gulp.task('build', gulp.series('sass'));
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+    open: true // This option will automatically open the browser for you
+  });
 
-gulp.task('watch', function() {
-  gulp.watch('assets/scss/*.scss', gulp.series('sass'));
+  gulp.watch('assets/scss/*.scss', gulp.series('sass')); // Path to Sass files
+  gulp.watch('*.html').on('change', browserSync.reload); // Path to HTML files
 });
 
-gulp.task('default', gulp.series('build', 'watch'));
+gulp.task('build', gulp.series('sass')); // Build task
+
+gulp.task('default', gulp.series('serve'));
