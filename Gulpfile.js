4629bfc9666568d8e3c9b511dacd227ef5173202
xada <wahabzada00@gmail.com>
2023-06-12 13:56:36 +0300

    "modify the Gulpfile foe netlify deployment"

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,27 +1,50 @@
+// const gulp = require('gulp');
+// const sass = require('gulp-sass')(require('sass'));
+// const sourcemaps = require('gulp-sourcemaps');
+// const browserSync = require('browser-sync').create();
+
+// gulp.task('sass', function() {
+//   return gulp.src('assets/scss/*.scss') // Path to Sass files
+//     .pipe(sourcemaps.init())
+//     .pipe(sass())
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest('assets/css')) // Path to output directory for CSS files
+//     .pipe(browserSync.stream());
+// });
+
+// gulp.task('serve', function() {
+//   browserSync.init({
+//     server: {
+//       baseDir: './'
+//     },
+//     open: true // This option will automatically open the browser for you
+//   });
+
+//   gulp.watch('assets/scss/*.scss', gulp.series('sass')); // Path to Sass files
+//   gulp.watch('*.html').on('change', browserSync.reload); // Path to HTML files
+// });
+
+// gulp.task('default', gulp.series('serve'));
+
+
+
+
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-  return gulp.src('assets/scss/*.scss') // Path to Sass files
+  return gulp.src('assets/scss/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('assets/css')) // Path to output directory for CSS files
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest('assets/css'));
 });
 
-gulp.task('serve', function() {
-  browserSync.init({
-    server: {
-      baseDir: './'
-    },
-    open: true // This option will automatically open the browser for you
-  });
+gulp.task('build', gulp.series('sass'));
 
-  gulp.watch('assets/scss/*.scss', gulp.series('sass')); // Path to Sass files
-  gulp.watch('*.html').on('change', browserSync.reload); // Path to HTML files
+gulp.task('watch', function() {
+  gulp.watch('assets/scss/*.scss', gulp.series('sass'));
 });
 
-gulp.task('default', gulp.series('serve'));
+gulp.task('default', gulp.series('build', 'watch'));
